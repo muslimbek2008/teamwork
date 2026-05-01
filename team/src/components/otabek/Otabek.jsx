7c812361bebacbe090faea13810ba61f99cc567e
Otabek <abdullayevriza80@gmail.com>
2026-05-01 16:11:10 +0500

    salom

--- team/src/components/otabek/Otabek.jsx
+++ team/src/components/otabek/Otabek.jsx
@@ -1,7 +1,17 @@
 
-const Otabek = () => {
+const Otabek = ({vector, vectorr , okam , paragrf}) => {
   return (
-    <div>Otabek</div>
+    <div className="flex items-center justify-between container h-[600px]">
+      <img src={vector} alt="icon" />
+
+      <div className="flex  gap-[55px]">
+        <img src={okam} alt="image" />
+
+        <p className="font-[400] text-[#FFFFFFC7] text-[14px] w-[400px] mt-[120px]">{paragrf}</p>
+      </div>
+
+      <img src={vectorr} alt="icon" />
+    </div>
   )
 }
 
