4f13a5d189bcf5f0b2c4711a768b712d5c496771
shoxruh01 <saydaxmadovshoxruh49@gmail.com>
2026-05-01 17:22:24 +0500

    lolahon2

--- team/src/App.jsx
+++ team/src/App.jsx
@@ -7,13 +7,44 @@ import Oysha from './components/oysha/Oysha'
 import Omina from './components/omina/Omina'
 
 
+const tools = [
+  {
+    id: 1,
+    title: "Zeplin",
+    price: "Free & Paid",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  ",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQsroxnqgWp_NBLzN3OsNu3OD6UPHDnJgumQ&s",
+  },
+  {
+    id: 2,
+    title: "PHPStorm",
+    price: "Free",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    img: "https://img.freepik.com/premium-psd/3d-photoshop-icon_913262-193.jpg?semt=ais_hybrid&w=740&q=80",
+  },
+  {
+    id: 3,
+    title: "Toolbox",
+    price: "Free",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    img: "https://img.freepik.com/free-photo/3d-rendering-holographic-cube_23-2150979698.jpg?semt=ais_hybrid&w=740&q=80",
+  },
+  {
+    id: 4,
+    title: "Procreate",
+    price: "Paid",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    img: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Procreate_icon.png/250px-Procreate_icon.png",
+  },
+];
+
 const App = () => {
   return (
     <div>
       <Muslimbek/> 
       <Alisher/>
       <Bahriddin/>
-      <Shohruhbek/> 
+      <Shohruhbek tools={tools} />
       <Otabek/>
       <Oysha/>
       <Omina/>
