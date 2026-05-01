b3f804a76f70479097e43dfb1b6c5980e420704d
ominaxon7 <abobakirovaominaxon@gmail.com>
2026-05-01 16:35:30 +0500

    wert

--- team/src/App.jsx
+++ team/src/App.jsx
@@ -8,11 +8,27 @@ import Omina from './components/omina/Omina'
 import vector from "./assets/Vector.png"
 import vectorr from "./assets/Vectorr.png"
 import okam from "./assets/okam.png"
+import logo from './assets/logo.png'
 
 
 // otabek
 const paragrf="Incididunt cillum do sint sint enim ullamco id deserunt mollit qui reprehenderit do. Velit ex tempor cillum ad sint occaecat. Do nulla velit labore occaecat do deserunt Lorem magna officia incidsdidunt consectetur amet. Sunt consectetur veniam minim ex commodo sint non. Occaecat aute officia excteur non laboris id qui ad."
 
+
+const footer_menu = [
+  {
+    title: "Contact Us",
+    links: ["+621987463", "M Building, No.10 A", "antools@awesome.com"]
+  },
+  {
+    title: "Categories",
+    links: ["Design", "Development"]
+  },
+  {
+    title: "Company Info",
+    links: ["About Us", "Our Partners", "Blog"]
+  }
+];
 const App = () => {
   return (
     <div>
@@ -22,7 +38,7 @@ const App = () => {
       <Shohruhbek/> 
       <Otabek vector={vector} vectorr={vectorr} okam={okam} paragrf={paragrf}/>
       <Oysha/>
-      <Omina/>
+      <Omina logo={logo} menu={footer_menu} />
     </div>
   )
 }
---
+++ team/src/components/omina/Omina.jsx
@@ -0,0 +1,40 @@
+const Omina = ({ logo, menu }) => {
+  return (
+    <footer className="bg-[#1E252B] py-16 text-white">
+      <div className="max-w-[1200px] mx-auto px-5 flex flex-wrap justify-between gap-10">
+        <div className="w-[300px] flex flex-col gap-6">
+          <img src={logo} alt="Logo" className="w-[140px] h-[38px]" />
+
+          <div className="flex flex-col gap-3">
+            <h3 className="font-bold text-[16px]">Copyright 2021. Antools</h3>
+            <p className="text-[14px] opacity-50">
+              Antool is a web collection of information on paid or free Design
+              and Development tools
+            </p>
+          </div>
+        </div>cd 
+        <div className="flex flex-wrap gap-20">
+          {menu?.map((item, index) => (
+            <div key={index} className="flex flex-col gap-6">
+              <h4 className="font-bold text-[18px]">{item.title}</h4>
+              <ul className="flex flex-col gap-4">
+                {item.links.map((link, i) => (
+                  <li key={i}>
+                    <a
+                      href="#"
+                      className="text-[16px] opacity-60 hover:opacity-100"
+                    >
+                      {link}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Omina;
